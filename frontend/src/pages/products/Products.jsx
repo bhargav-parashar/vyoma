986002565358f40f6dd81e73332d40e2f533c6f9
Bhargav Parashar <bhargavparashar99@gmail.com>
2025-05-04 23:57:56 +0530

    feat:header layout

---
+++ frontend/src/pages/products/Products.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div>
+      <div style={{ border: "2px solid pink", width: "100vw" }}>
+        <p style={{color:'black'}} >Header</p>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
