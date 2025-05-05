0f1b629f1439e0d499def465873e4ae2f647950d
Bhargav Parashar <bhargavparashar99@gmail.com>
2025-05-05 23:56:30 +0530

    feat: Header changes continued

--- frontend/src/pages/products/Products.jsx
+++ frontend/src/pages/products/Products.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Products = () => {
   return (
     <div>
-      <div style={{ border: "2px solid pink", width: "100vw" }}>
+      <div style={{ border: "2px solid pink", background:'grey', display:'flex', justifyContent:'space-between'}}>
         <p style={{color:'black'}} >Header</p>
       </div>
     </div>
