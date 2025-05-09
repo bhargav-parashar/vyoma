0d2728a88d0739f947d68f1497a41592e398ea8d
Bhargav Parashar <bhargavparashar99@gmail.com>
2025-05-09 23:58:20 +0530

    feat: products page structure

--- frontend/src/pages/products/Products.jsx
+++ frontend/src/pages/products/Products.jsx
@@ -3,8 +3,18 @@ import React from "react";
 const Products = () => {
   return (
     <div>
-      <div style={{ border: "2px solid pink", background:'grey', display:'flex', justifyContent:'space-between'}}>
-        <p style={{color:'black'}} >Header</p>
+      <div className="flex justify-between bg-pink-100">
+        <p>Header</p>
+        <p>Header</p>
+      </div>
+      <div className="h-screen flex">
+        <aside className="w-1/4 bg-gray-100 p-4 border-r">
+          <h2 className="text-xl font-semibold mb-4">Filters</h2>
+        </aside>
+
+        <main className="w-3/4 p-4 overflow-y-auto">
+          <h2 className="text-xl font-semibold mb-4">Products</h2>
+        </main>
       </div>
     </div>
   );
