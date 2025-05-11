804b8d56a5f4a2fc3d87774afd21c308b15e79fe
Bhargav Parashar <bhargavparashar99@gmail.com>
2025-05-11 23:55:18 +0530

    feat: Sidebar filter 2

---
+++ frontend/src/components/priceSlider.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const priceSlider = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default priceSlider
--- frontend/src/pages/products/Products.jsx
+++ frontend/src/pages/products/Products.jsx
@@ -8,31 +8,55 @@ const Products = () => {
         <p>Header</p>
       </div>
       <div className="flex flex-1 overflow-hidden">
-        <aside className="w-1/5 bg-gray-100 border-r">
-          <h2 className="text-xl font-semibold  p-4 ">FILTERS</h2>
-          <hr />
+        <aside className="w-1/5 border-r border-gray-400">
+          <h2 className="text-sm font-bold  p-4 ">FILTERS</h2>
+          <hr className="text-gray-400 " />
           <div className="p-4">
-            <h3 className="text-sm font-semibold mb-4">CATEGORIES</h3>
+            <h3 className="text-xs font-bold mb-4">CATEGORIES</h3>
             <div>
               <label className="inline-flex items-center text-sm">
-                <input
-                  type="radio"
-                  name="option"
-                  value="option1"
-                  className="form-radio text-blue-600"
-                />
+                <input type="checkbox" />
                 <span className="ml-2">Tshirts</span>
               </label>
             </div>
             <div>
               <label className="inline-flex items-center text-sm">
-                <input
-                  type="radio"
-                  name="option"
-                  value="option2"
-                  className="form-radio text-blue-600"
-                />
+                <input type="checkbox" />
                 <span className="ml-2">Lounge Tshirts</span>
+              </label>
+            </div>
+          </div>
+          <hr className="text-gray-400 " />
+          <div className="p-4">
+            <h3 className="text-xs font-bold mb-4">BRANDS</h3>
+            <div>
+              <label className="inline-flex items-center text-sm">
+                <input type="checkbox" />
+                <span className="ml-2">Allen Solly</span>
+              </label>
+            </div>
+            <div>
+              <label className="inline-flex items-center text-sm">
+                <input type="checkbox" />
+                <span className="ml-2">ADIDAS</span>
+              </label>
+            </div>
+            <div>
+              <label className="inline-flex items-center text-sm">
+                <input type="checkbox" />
+                <span className="ml-2">Blackberrys</span>
+              </label>
+            </div>
+            <div>
+              <label className="inline-flex items-center text-sm">
+                <input type="checkbox" />
+                <span className="ml-2">Being Human</span>
+              </label>
+            </div>
+            <div>
+              <label className="inline-flex items-center text-sm">
+                <input type="checkbox" />
+                <span className="ml-2">Cantabil</span>
               </label>
             </div>
           </div>
