416d96bffa1d39bc33db35505313f9d510365fe4
Bhargav Parashar <bhargavparashar99@gmail.com>
2025-05-19 23:58:13 +0530

    feat: product card modification

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProductCard = () => {
   return (
-    <div className="border border-l-amber-950 h-70 w-50">
+    <div className="border border-l-amber-950 h-70 w-50 bg-pink-200">
       <h1>products</h1>
     </div>
   );
