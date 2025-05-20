61fcdbc37656ffb75bb5cf2500dbaf764b9e5dfc
Bhargav Parashar <bhargavparashar99@gmail.com>
2025-05-20 23:58:34 +0530

    feat: assets added

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import card from "../../assets/product-image.jpg";
 
 const ProductCard = () => {
   return (
     <div className="border border-l-amber-950 h-70 w-50 bg-pink-200">
       <h1>products</h1>
+      <img src="../../assets/product-image.jpg"  />
     </div>
   );
 };
