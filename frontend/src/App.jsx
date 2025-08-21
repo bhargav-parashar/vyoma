19ae5f9dbf84b8923dd8b64b95a35e1b483e3920
Bhargav Parashar <bhargavparashar99@gmail.com>
2025-08-21 23:58:42 +0530

    update: breadcrumbs layout test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import { FilterContextProvider } from "./contexts/FiltersContext";
 import { SnackbarProvider } from "./contexts/SnackbarContext";
 import { useSelector } from "react-redux";
+import Breadcrumbs from "./components/Breadcrumbs/Breadcrumbs";
 
 function App() {
   const currTheme = useSelector((store) => store.theme.currTheme);
@@ -13,6 +14,7 @@ function App() {
       <FilterContextProvider>
         <div className={`${currTheme == `dark` ? `dark` : ``}`}>
           <Header />
+          <Breadcrumbs/>
           <Outlet />
         </div>
       </FilterContextProvider>
--- frontend/src/components/Breadcrumbs/Breadcrumbs.jsx
+++ frontend/src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { useSearchParams } from "react-router-dom";
 
 const Breadcrumbs = () => {
+  //MAINTAIN STATE FOR SEARCH PARAMS
+  const [searchParams, setSearchParams] = useSearchParams();
+    console.log(searchParams);
   return (
-    <div>
+    <div >
       
     </div>
   )
